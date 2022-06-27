90ca36e6775e21c6167d2dadedd054b8ed1b32c2
unknown <abhisheksrivastava9719@gmail.com>
2022-06-27 20:47:02 +0530

    Maximum of three integers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Genericss;
+Console.WriteLine("Welcome to generics problem");
+Console.WriteLine("Enter first integer value");
+int e =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second integer value");
+int f = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third integer value");
+int g = Convert.ToInt32(Console.ReadLine());
+maximum.max(e,f,g);
---
+++ maximum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericss
+{
+    public class maximum
+    {
+        public static void max(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a + " is the greatest");
+            }
+            else if (b>a && b>c)
+            {
+                Console.WriteLine(b + " is the greatest");
+            }
+            else if (c>a && c>b)
+            {
+                Console.WriteLine(c + " is the greatest");
+            }
+            else
+            {
+                Console.WriteLine("All three are equal");
+            }
+        }
+    }
+}
